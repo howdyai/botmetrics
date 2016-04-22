06d4d326eda3bf576579ae815c881b8710de0660
arunthampi <arun.thampi@gmail.com>
2016-04-22 15:24:45 -0700

    Allow user to create bot instance

--- app/controllers/bot_instances_controller.rb
+++ app/controllers/bot_instances_controller.rb
@@ -1,2 +1,34 @@
 class BotInstancesController < ApplicationController
+  before_filter :authenticate_user!
+  before_filter :find_team
+  before_filter :find_bot
+
+  def new
+    @instance = @bot.instances.build
+  end
+
+  def create
+    @instance = @bot.instances.create(instance_params)
+
+    if @instance.persisted?
+      redirect_to team_bot_path(@team, @bot)
+    else
+      render :new
+    end
+  end
+
+  protected
+  def find_team
+    @team = current_user.teams.find_by(uid: params[:team_id])
+    raise ActiveRecord::RecordNotFound if @team.blank?
+  end
+
+  def find_bot
+    @bot = @team.bots.find_by(uid: params[:bot_id])
+    raise ActiveRecord::RecordNotFound if @bot.blank?
+  end
+
+  def instance_params
+    params.require(:instance).permit(:token, :uid)
+  end
 end
--- spec/controllers/bot_instances_controller_spec.rb
+++ spec/controllers/bot_instances_controller_spec.rb
@@ -1,5 +1,51 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe BotInstancesController, type: :controller do
+describe BotInstancesController do
+  let!(:user) { create :user }
+  let!(:team) { create :team }
+  let!(:bot)  { create :bot, team: team }
+  let!(:tm1)  { create :team_membership, team: team, user: user }
 
+  describe 'GET new' do
+    before { sign_in user }
+
+    def do_request
+      get :new, team_id: team.to_param, bot_id: bot.to_param
+    end
+
+    it 'should render template :new' do
+      do_request
+      expect(response).to render_template :new
+    end
+
+    it "should set instance variable '@instance'" do
+      do_request
+      expect(assigns(:instance)).to_not be_nil
+    end
+  end
+
+  describe 'POST create' do
+    before { sign_in user }
+    let!(:bot_instance_params) { { token: 'token-deadbeef', uid: 'UNESTOR1' } }
+
+    def do_request
+      post :create, instance: bot_instance_params, team_id: team.to_param, bot_id: bot.to_param
+    end
+
+    it 'should create a new bot instance' do
+      expect {
+        do_request
+        bot.reload
+      }.to change(bot.instances, :count).by(1)
+
+      instance = bot.instances.last
+      expect(instance.token).to eql 'token-deadbeef'
+      expect(instance.uid).to eql 'UNESTOR1'
+    end
+
+    it "should redirect back to team_bot_path" do
+      do_request
+      expect(response).to redirect_to team_bot_path(team, bot)
+    end
+  end
 end
