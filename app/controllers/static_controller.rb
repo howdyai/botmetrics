0f25ff29c98545c1237472b75ef1a0f40765cf33
arunthampi <arun.thampi@gmail.com>
2016-04-26 11:20:28 -0700

    Add letsencrypt endpoint

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,4 +3,8 @@ class StaticController < ApplicationController
 
   def index
   end
+
+  def letsencrypt
+    render text: 'TVAQ1mHoZykrk40sVCNFpGIc03MSDajgp-e758SpG4w.Xxo_ImoX_5mqmLIsmpF8ecjbaPa8GmVSipNI5wpr0uo'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   end
 
   root 'static#index'
+
+  get '/.well-known/acme-challenge/:id' => 'static#letsencrypt'
 end
