267e772f467176fbdbbf55be1a55b496fea72f84
arunthampi <arun.thampi@gmail.com>
2016-05-09 11:37:39 -0700

    Temporarily put back associations for migrations to run :(

---
+++ app/models/team.rb
@@ -0,0 +1,21 @@
+class Team < ActiveRecord::Base
+  before_validation :set_uid!
+
+  validates_presence_of :name
+  validates_uniqueness_of :uid
+
+  has_many :team_memberships
+  has_many :members, through: :team_memberships, source: :user
+  has_many :owners, -> { where("team_memberships.membership_type = ?", 'owner') }, through: :team_memberships, source: :user
+
+  has_many :bots
+
+  def to_param
+    self.uid
+  end
+
+  protected
+  def set_uid!
+    self.uid = SecureRandom.hex(6) if self.uid.blank?
+  end
+end
