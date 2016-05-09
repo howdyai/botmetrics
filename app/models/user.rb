8fccefe3a33d61a6b3da4f99fab009c838f19745
arunthampi <arun.thampi@gmail.com>
2016-05-09 11:43:09 -0700

    Revert "Temporarily put back associations for migrations to run :("
    This reverts commit 5b9dfd93f639719c9ebbeab0adfba943b2450863.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,6 @@ class User < ActiveRecord::Base
 
   has_many :bot_collaborators
   has_many :bots, through: :bot_collaborators
-  has_many :team_memberships
-  has_many :teams, through: :team_memberships
 
   validates_uniqueness_of :api_key, if: Proc.new { |u| u.api_key.present? }
 
