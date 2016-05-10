b28dc69c26ad1f540a3a1572f48f0aa43c07274c
arunthampi <arun.thampi@gmail.com>
2016-05-10 10:14:45 -0700

    Disable active_record logging in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Disable activerecord logger
+  config.active_record.logger = nil
 end
