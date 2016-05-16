b9d79e31c05642b4d256129697dde9df513680cd
JuanitoFatas <katehuang0320@gmail.com>
2016-05-16 14:17:14 +0800

    Add specs to GetStartEnd for timezone and time range

--- app/lib/get_start_end.rb
+++ app/lib/get_start_end.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
 class GetStartEnd
-  def initialize(start_date, end_date, user_time_zone = 'UTC')
-    @start_date = start_date
-    @end_date = end_date
+  def initialize(start_time, end_time, user_time_zone = 'UTC')
+    @start_time = start_time
+    @end_time = end_time
     @user_time_zone = user_time_zone
   end
 
   def call
-    start = start_date.to_s.in_time_zone(user_time_zone)
+    start = start_time.to_s.in_time_zone(user_time_zone)
     start = (Time.current - 6.days).in_time_zone(user_time_zone) if start.blank?
-    _end  = end_date.to_s.in_time_zone(user_time_zone)
+    _end  = end_time.to_s.in_time_zone(user_time_zone)
     _end  = start + 6.days if _end.blank?
 
     start = start.beginning_of_day
@@ -21,5 +21,5 @@ class GetStartEnd
 
   private
 
-    attr_reader :start_date, :end_date, :user_time_zone
+    attr_reader :start_time, :end_time, :user_time_zone
 end
--- spec/models/lib/get_start_end_spec.rb
+++ spec/models/lib/get_start_end_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe GetStartEnd do
   describe '#call' do
-    before { travel_to Time.new(2016, 5, 1) }
+    before { travel_to Time.new(2016, 5, 16) }
+    after { travel_back }
 
     it 'returns six days ago and end of today if no arguments given' do
       result = described_class.new(nil, nil).call
@@ -11,6 +12,25 @@ RSpec.describe GetStartEnd do
       expect(result).to match_array([start_time.beginning_of_day, end_time.end_of_day])
     end
 
-    after { travel_back }
+    it 'respects user time zone' do
+      user_time_zone = 'Pacific Time (US & Canada)'
+      result = described_class.new(nil, nil, user_time_zone).call
+
+      start_time = (Time.current - 6.days).in_time_zone(user_time_zone)
+      end_time = (start_time + 6.days).in_time_zone(user_time_zone)
+
+      expect(result).to match_array([start_time.beginning_of_day, end_time.end_of_day])
+    end
+
+    it 'respects passed in time' do
+      start_time = Time.new(2016, 5, 1)
+      end_time = Time.new(2016, 5, 14)
+      result = described_class.new(start_time, end_time).call
+
+      start_time = start_time.in_time_zone('UTC')
+      end_time = end_time.in_time_zone('UTC')
+
+      expect(result).to match_array([start_time.beginning_of_day, end_time.end_of_day])
+    end
   end
 end
