2545bf5fc5a32efb521a1628b2d8b7f2d18c7e9b
JuanitoFatas <katehuang0320@gmail.com>
2016-05-16 14:57:54 +0800

    Add specs for GetBotInstancesCountByUnit

---
+++ spec/models/lib/get_bot_instances_count_by_unit_spec.rb
@@ -0,0 +1,25 @@
+RSpec.describe GetBotInstancesCountByUnit do
+  describe '#call' do
+    context 'day' do
+      let(:unit) { 'day' }
+
+      it 'works' do
+        travel_to Time.current do
+          create :bot_instance
+          create :bot_instance, created_at: Time.current + 6.days
+
+          create :bot_instance, created_at: Time.current.yesterday
+          create :bot_instance, created_at: Time.current + 7.days
+
+          result = begin
+            GetBotInstancesCountByUnit.new(
+              unit, BotInstance.all, Time.current, Time.current + 6.days, 'UTC'
+            ).call
+          end
+
+          expect(result.values).to match_array [1, 0, 0, 0, 0, 0, 1]
+        end
+      end
+    end
+  end
+end
