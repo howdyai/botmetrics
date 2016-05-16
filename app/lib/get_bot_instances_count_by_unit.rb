0bbdebc2c49dfbeb226d4537921e11c44c65bd22
JuanitoFatas <katehuang0320@gmail.com>
2016-05-16 15:05:02 +0800

    Add support to any kind of instances

--- app/lib/get_bot_instances_count_by_unit.rb
+++ app/lib/get_bot_instances_count_by_unit.rb
@@ -12,7 +12,7 @@ class GetBotInstancesCountByUnit
   def call
     instances.send(
       :"group_by_#{unit}",
-      'bot_instances.created_at',
+      "#{instances.table_name}.created_at",
       range: start_time..end_time,
       time_zone: user_time_zone
     ).count
