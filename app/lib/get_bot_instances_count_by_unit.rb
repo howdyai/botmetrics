d97f5fe54dcdcc5863d3abbe122b3437d809b0c5
JuanitoFatas <katehuang0320@gmail.com>
2016-05-16 15:02:26 +0800

    Refactor into one method chain

--- app/lib/get_bot_instances_count_by_unit.rb
+++ app/lib/get_bot_instances_count_by_unit.rb
@@ -10,14 +10,12 @@ class GetBotInstancesCountByUnit
   end
 
   def call
-    case unit
-    when 'day'
-      instances.group_by_day("bot_instances.created_at", range: start_time..end_time, time_zone: user_time_zone).count
-    when 'week'
-      instances.group_by_week("bot_instances.created_at", range: start_time..end_time, time_zone: user_time_zone).count
-    when 'month'
-      instances.group_by_month("bot_instances.created_at", range: start_time..end_time, time_zone: user_time_zone).count
-    end
+    instances.send(
+      :"group_by_#{unit}",
+      'bot_instances.created_at',
+      range: start_time..end_time,
+      time_zone: user_time_zone
+    ).count
   end
 
   private
