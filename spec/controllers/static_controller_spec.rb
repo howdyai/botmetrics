7d275da6b3cb34d5606bfa478fa6c7fe534869b9
JuanitoFatas <katehuang0320@gmail.com>
2016-05-17 19:05:28 +0800

    Add missing spec content for StaticController

--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe StaticController, type: :controller do
+RSpec.describe StaticController do
+  describe 'GET index' do
+    let!(:user) { create :user }
+    let!(:bot)  { create :bot }
+    let!(:bc1)  { create :bot_collaborator, bot: bot, user: user }
 
+    before do
+      sign_in user
+      do_request
+    end
+
+    def do_request
+      get :index
+    end
+
+    it 'redirects' do
+      expect(response).to redirect_to bot_path(user.bots.first)
+    end
+  end
 end
