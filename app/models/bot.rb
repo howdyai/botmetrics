c0354aab6b399b2b740b9222309ee3a75c949da1
JuanitoFatas <katehuang0320@gmail.com>
2016-05-24 17:16:03 +0800

    Refactor into WithUidUniqueness for reuse

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -1,8 +1,7 @@
 class Bot < ActiveRecord::Base
-  before_validation :set_uid!
+  include WithUidUniqueness
 
   validates_presence_of   :name, :provider
-  validates_uniqueness_of :uid
   validates_inclusion_of  :provider, in: %w(slack kik facebook telegram)
 
   has_many :instances, class_name: 'BotInstance'
@@ -12,13 +11,4 @@ class Bot < ActiveRecord::Base
   has_many :owners, -> { where("bot_collaborators.collaborator_type" => 'owner') }, through: :bot_collaborators, source: :user
 
   has_many :notifications
-
-  def to_param
-    self.uid
-  end
-
-  protected
-  def set_uid!
-    self.uid = SecureRandom.hex(6) if self.uid.blank?
-  end
 end
---
+++ app/models/concerns/with_uid_uniqueness.rb
@@ -0,0 +1,17 @@
+module WithUidUniqueness
+  extend ActiveSupport::Concern
+
+  included do
+    before_validation :set_uid!
+    validates_uniqueness_of :uid
+  end
+
+  def to_param
+    self.uid
+  end
+
+  protected
+  def set_uid!
+    self.uid = SecureRandom.hex(6) if self.uid.blank?
+  end  
+end
