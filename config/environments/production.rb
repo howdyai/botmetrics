8e5d1c82e114d1a287b2c7d3859b8da739d6400c
arunthampi <arun.thampi@gmail.com>
2016-05-26 14:53:35 -0700

    Add Sendgrid config to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,14 @@ Rails.application.configure do
 
   # Disable activerecord logger
   config.active_record.logger = nil
+
+  ActionMailer::Base.smtp_settings = {
+    address:        'smtp.sendgrid.net',
+    port:           '587',
+    authentication: :plain,
+    user_name:      ENV['SENDGRID_USERNAME'],
+    password:       ENV['SENDGRID_PASSWORD'],
+    domain:         'heroku.com',
+    enable_starttls_auto: true
+  }
 end
