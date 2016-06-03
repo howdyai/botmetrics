bbc1d376205543df83b775f9556c77fe00203c5a
Winston <winston.yongwei@gmail.com>
2016-06-03 11:59:10 +0800

    Make daily reports send at 9:05am instead.

--- clockwork.rb
+++ clockwork.rb
@@ -7,7 +7,7 @@ module Clockwork
     SendScheduledMessageJob.perform_async
   end
 
-  every(1.hour, 'DailyReport.Send', at: ['**:00']) do
+  every(1.hour, 'DailyReport.Send', at: ['**:05']) do
     SendDailyReportsJob.perform_async
   end
 end
