4ac1ff356ee7f18edb954977cd82fc0e341c75f8
JuanitoFatas <katehuang0320@gmail.com>
2016-06-06 16:02:10 +0800

    Use create_singaporean helper in spec

--- spec/services/daily_reports_service_spec.rb
+++ spec/services/daily_reports_service_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe DailyReportsService do
     end
 
     it 'unsubscribed user' do
-      create(:user, timezone: 'Asia/Singapore', email_preferences: { daily_reports: '0' })
+      create_singaporean(email_preferences: { daily_reports: '0' })
 
       travel_to Time.parse('May 20, 2016 09:00 +0800') do
         service.send_now
@@ -35,9 +35,7 @@ RSpec.describe DailyReportsService do
 
     it 'sent still in 24-hour window, subscribed, 9 am (in user timezone)' do
       travel_to Time.parse('May 20, 2016 09:00 +0800') do
-        create(
-          :user,
-          timezone: 'Asia/Singapore',
+        create_singaporean(
           email_preferences: { daily_reports: '1' },
           tracking_attributes: { last_daily_summary_sent_at: (24.hours.ago + 1.second).to_i }
         )
