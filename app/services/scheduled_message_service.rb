73c392aafde0025abdedb535c3beabb21d7d0281
JuanitoFatas <katehuang0320@gmail.com>
2016-06-06 18:37:40 +0800

    Refactor ScheduledMessageService

--- app/services/scheduled_message_service.rb
+++ app/services/scheduled_message_service.rb
@@ -1,13 +1,9 @@
 class ScheduledMessageService
   def send_now
-    nearest_time = NearestTime.round(Time.current)
-
     Message.scheduled.find_each do |message|
-      if message.can_send_now?(nearest_time)
-        Rails.logger.info "[SendScheduledMessageJob] Sending Message for ID #{message.id}"
+      next unless message.can_send_now?
 
-        SendMessageJob.perform_async(message.id)
-      end
+      SendMessageJob.perform_async(message.id)
     end
   end
 end
--- spec/services/scheduled_message_service_spec.rb
+++ spec/services/scheduled_message_service_spec.rb
@@ -1,46 +1,45 @@
 RSpec.describe ScheduledMessageService do
   describe '#perform' do
-    let!(:scheduled_messages) do
-      create_list(:message, 2, :to_user, sent_at: nil, scheduled_at: scheduled_at)
-    end
+    let(:scheduled_at) { Time.zone.parse('2016-06-06 08:00 +0800') }
+    let(:next_day) { Time.zone.parse('2016-06-07 08:00 +0800') }
 
-    let(:scheduled_at) { Time.parse('2016-05-15 08:00 +0200') }
+    def perform
+      described_class.new.send_now
+    end
 
     before do
-      create(:message, :to_user, sent_at: Time.current,  scheduled_at: nil)
-      create(:message, :to_user, sent_at: nil, scheduled_at: nil)
       create(:message, :to_user, sent_at: Time.current,  scheduled_at: scheduled_at)
-      create(:message, :to_user, sent_at: nil, scheduled_at: Time.parse('2016-05-15 08:00 +0800'))
+      create(:message, :to_user, sent_at: nil, scheduled_at: scheduled_at)
+      create(:message, :to_user, sent_at: nil, scheduled_at: next_day)
     end
 
+
     context 'wrong timing' do
-      before { travel_to Time.parse('2016-05-15 08:05 +0400') }
+      before { travel_to Time.zone.parse('2016-06-06 08:05 +1200') }
 
       it 'does not send anything' do
         allow(SendMessageJob).to receive(:perform_async)
 
-        ScheduledMessageService.new.send_now
+        perform
 
         expect(SendMessageJob).to have_received(:perform_async).exactly(0).times
-        scheduled_messages.each do |message|
-          expect(SendMessageJob).to_not have_received(:perform_async).with(message.id)
-        end
       end
+
+      after { travel_back }
     end
 
     context 'exact timing' do
-      before { travel_to Time.parse('2016-05-15 08:05 +0200') }
+      before { travel_to Time.parse('2016-06-06 08:05 +0800') }
 
       it 'sends for scheduled message only' do
         allow(SendMessageJob).to receive(:perform_async)
 
-        ScheduledMessageService.new.send_now
+        perform
 
-        expect(SendMessageJob).to have_received(:perform_async).twice
-        scheduled_messages.each do |message|
-          expect(SendMessageJob).to have_received(:perform_async).with(message.id)
-        end
+        expect(SendMessageJob).to have_received(:perform_async).once
       end
+
+      after { travel_back }
     end
   end
 end
