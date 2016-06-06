b91df1db72e5a72cfc2a7410dc6edfe3f783da4d
JuanitoFatas <katehuang0320@gmail.com>
2016-06-06 17:07:55 +0800

    Simplify SendMessageJob

--- app/jobs/send_message_job.rb
+++ app/jobs/send_message_job.rb
@@ -1,25 +1,5 @@
 class SendMessageJob < Job
   def perform(message_id)
-    message = Message.find(message_id)
-    MessageService.new(message).send_now
-
-    ping_pusher(message)
+    MessageService.new(message_id).send_now
   end
-
-  private
-
-    def ping_pusher(message)
-      notification = message.notification
-      if notification
-        PusherJob.perform_async(
-          "notification",
-          "notification-#{notification.id}",
-          {
-            ok: message.success,
-            recipient: (message.user || message.channel),
-            sent: notification.messages.sent.count
-          }.to_json
-        )
-      end
-    end
 end
--- spec/jobs/send_message_job_spec.rb
+++ spec/jobs/send_message_job_spec.rb
@@ -2,42 +2,10 @@ require 'spec_helper'
 
 RSpec.describe SendMessageJob do
   describe '#perform' do
-    let(:mocked_service) { double(:message_service).as_null_object }
-
-    before do
-      allow(MessageService).to receive(:new) { mocked_service }
-      allow(mocked_service).to receive(:send_now) { true }
-    end
-
-    let(:message)  { create(:message, :to_user) }
-
     it 'sends message' do
-      SendMessageJob.new.perform(message.id)
+      expect(MessageService).to receive_message_chain(:new, :send_now) { true }
 
-      expect(MessageService).to have_received(:new).with(message)
-      expect(mocked_service).to have_received(:send_now)
-    end
-
-    context 'message belongs to a notification' do
-      let(:notification) { create(:notification) }
-      let(:message)      { create(:message, :to_user, notification: notification) }
-
-      before { allow(PusherJob).to receive(:perform_async) }
-
-      it 'invokes PusherJob' do
-        SendMessageJob.new.perform(message.id)
-
-        expect(PusherJob).to have_received(:perform_async).
-          with(
-            "notification",
-            "notification-#{notification.id}",
-            {
-              ok: message.success,
-              recipient: message.user,
-              sent: notification.messages.sent.count
-            }.to_json
-        )
-      end
+      SendMessageJob.new.perform(42)
     end
   end
 end
