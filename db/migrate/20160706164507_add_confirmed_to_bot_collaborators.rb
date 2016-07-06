404eb33c4dbf29e248c24b290001410df9d2dbd9
arunthampi <arun.thampi@gmail.com>
2016-07-06 14:17:53 -0700

    reset column information before updating attributes

--- db/migrate/20160706164507_add_confirmed_to_bot_collaborators.rb
+++ db/migrate/20160706164507_add_confirmed_to_bot_collaborators.rb
@@ -1,6 +1,8 @@
 class AddConfirmedToBotCollaborators < ActiveRecord::Migration
   def up
     add_column :bot_collaborators, :confirmed_at, :datetime
+    BotCollaborator.reset_column_information
+
     BotCollaborator.find_each do |bc|
       bc.update_attribute(:confirmed_at, bc.user.created_at)
     end
