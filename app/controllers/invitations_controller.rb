66dd65a2efe84280d94266cf7d1304a8c8e5a804
arunthampi <arun.thampi@gmail.com>
2016-07-06 11:14:38 -0700

    Show the bots you have been invited to

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -5,11 +5,21 @@ class InvitationsController < Devise::InvitationsController
         bot = current_user.bots.find_by(uid: params[:invite][:bot_id])
         BotCollaborator.create!(bot: bot, user: resource, collaborator_type: 'member')
         TrackMixpanelEventJob.perform_async('Invited Collaborator to Bot', current_user.id, bot_id: bot.uid)
+
+        resource.deliver_invitation
       end
     end
   end
 
   protected
+  def invite_resource
+    ## skip sending emails on invite
+    ## They will be sent after the bot has been added to user
+    super do |u|
+      u.skip_invitation = true
+    end
+  end
+
   def invite_params
     i_params = params.require(:invite).permit(:full_name, :email, :timezone, :timezone_utc_offset, :bot_id)
 
