cfc69505fac43c82a2b9366900f65ae11068b7b8
arunthampi <arun.thampi@gmail.com>
2016-07-19 09:58:15 -0700

    Change letsencrypt challenge

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -11,6 +11,6 @@ class StaticController < ApplicationController
   end
 
   def letsencrypt
-    render text: 'TVAQ1mHoZykrk40sVCNFpGIc03MSDajgp-e758SpG4w.Xxo_ImoX_5mqmLIsmpF8ecjbaPa8GmVSipNI5wpr0uo'
+    render text: 'hIbdrX6Kkog-Jf9W94K5nJ6VXNUn8U61s-xKUn6Puss.46T4ETl3cFnTgHvAPLmpVqV3yv-8M6dJGWobu-kh0zw'
   end
 end
