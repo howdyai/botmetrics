200e38d803c01d523c6d54068e88dd80f8c8bb7c
arunthampi <arun.thampi@gmail.com>
2016-08-01 16:52:03 -0700

    Setup Cloudfront for assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = 'https://www.getbotmetrics.com'
+  config.action_controller.asset_host = 'https://d23damjyv3dh2i.cloudfront.net'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
