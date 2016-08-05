8d75fa1b4b5ca37233bee969148135595f4af3a0
arunthampi <arun.thampi@gmail.com>
2016-08-05 16:30:40 -0700

    Add constraints to check for presence of mid,seq in events

---
+++ db/migrate/20160805231725_add_mid_seq_constraints_on_events.rb
@@ -0,0 +1,45 @@
+class AddMidSeqConstraintsOnEvents < ActiveRecord::Migration
+  def up
+    execute "ALTER TABLE events DROP CONSTRAINT IF EXISTS validate_attributes_mid"
+    execute "ALTER TABLE events DROP CONSTRAINT IF EXISTS validate_attributes_seq"
+
+    execute "ALTER TABLE events ADD CONSTRAINT validate_attributes_mid
+                   CHECK (
+                          (
+                            (event_attributes->>'mid') IS NOT NULL
+                            AND length(event_attributes->>'mid') > 0
+                            AND provider = 'facebook'
+                            AND event_type = 'message'
+                          )
+                          OR
+                          (
+                            provider = 'facebook'
+                            AND event_type <> 'message'
+                            AND event_attributes IS NOT NULL
+                          )
+                          OR provider = 'slack'
+                         )"
+
+    execute "ALTER TABLE events ADD CONSTRAINT validate_attributes_seq
+                   CHECK (
+                          (
+                            (event_attributes->>'seq') IS NOT NULL
+                            AND length(event_attributes->>'seq') > 0
+                            AND provider = 'facebook'
+                            AND event_type = 'message'
+                          )
+                          OR
+                          (
+                            provider = 'facebook'
+                            AND event_type <> 'message'
+                            AND event_attributes IS NOT NULL
+                          )
+                          OR provider = 'slack'
+                         )"
+  end
+
+  def down
+    execute "ALTER TABLE events DROP CONSTRAINT IF EXISTS validate_attributes_mid"
+    execute "ALTER TABLE events DROP CONSTRAINT IF EXISTS validate_attributes_seq"
+  end
+end
