840dfb0f367a366f04e7a5630709a0c0dbae9ff9
vlad-shevtsov-mev <vlad.shevtsov@mev.com>
2016-08-10 17:22:53 +0300

    Add Kik api client.

--- app/controllers/bot_instances_controller.rb
+++ app/controllers/bot_instances_controller.rb
@@ -73,7 +73,7 @@ class BotInstancesController < ApplicationController
 
   protected
   def instance_params
-    params.require(:instance).permit(:token, :created_at)
+    params.require(:instance).permit(:token, :username, :created_at)
   end
 
   def update_instance_params
---
+++ app/lib/kik.rb
@@ -0,0 +1,47 @@
+require 'uri'
+
+class Kik
+  API_URL = ENV['KIK_API_URL']
+  OK = 200
+
+  def initialize(api_key, username)
+    @token = api_key
+    @username = username
+  end
+
+  def call(facebook_api, method, params = {}, &block)
+    params = params.select { |k,v| v.present? }
+    params.merge!(access_token: @token)
+    encoded_params = URI.encode_www_form(params)
+    auth_token = Base64.encode64("#{@username}:#{@token}").chop
+
+    opts = {
+      omit_default_port: true,
+      idempotent: true,
+      retry_limit: 6,
+      read_timeout: 360,
+      connect_timeout: 360,
+      headers: { 'Authorization': "Basic #{auth_token}" }
+    }
+
+    url = "#{API_URL}/#{facebook_api}"
+
+    if method.to_s.downcase == 'get'
+      url = "#{url}?#{encoded_params}"
+    else
+      opts[:body] = encoded_params
+      opts[:headers].merge("Content-Type": "application/x-www-form-urlencoded")
+    end
+
+
+    if !block_given?
+      connection = Excon.new(url, opts)
+      response = connection.request(method: method)
+      return JSON.parse(response.body).merge('status' => response.status)
+    else
+      opts[:response_block] = block
+      connection = Excon.new(url, opts)
+      connection.request(method: method)
+    end
+  end
+end
