ab55b245d6f57349c82d66f5cc215c90038cb0c4
arunthampi <arun.thampi@gmail.com>
2016-08-23 14:15:01 -0700

    Log Daily Summary Sent At

--- app/services/daily_reports_service.rb
+++ app/services/daily_reports_service.rb
@@ -5,6 +5,10 @@ class DailyReportsService
       next unless user.can_send_daily_summary?
 
       Rails.logger.info "[ReportsMailer] Sending Daily Report to #{user.id} #{user.email} at #{Time.now}"
+      if user.last_daily_summary_sent_at.present?
+        Rails.logger.info "[ReportsMailer] last_daily_summary_sent_at for #{user.id} #{user.email}: #{Time.at(user.last_daily_summary_sent_at)}"
+      end
+
       ReportsMailer.daily_summary(user.id).deliver_later
     end
   end
