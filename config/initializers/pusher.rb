87942e46a87bc800b123855f63563bbb5f69e0dc
arunthampi <arun.thampi@gmail.com>
2016-09-19 07:17:14 -0700

    Setup Pusher correctly
    when there are variable ways in which Pusher credentials can be presented to the user

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,6 +1,17 @@
-Pusher.app_id = Settings.pusher_app_id
-Pusher.key = Settings.pusher_api_key
-Pusher.secret = Settings.pusher_secret
+require 'uri'
+
+if (pusher_url = Settings.pusher_url).present?
+  Pusher.url = pusher_url
+  uri = URI.parse(pusher_url)
+
+  Pusher.app_id = uri.path.match(/apps\/(\d+)$/)[1]
+  Pusher.key = uri.user
+  Pusher.secret = uri.password
+else
+  Pusher.app_id = Settings.pusher_app_id
+  Pusher.key = Settings.pusher_api_key
+  Pusher.secret = Settings.pusher_secret
+end
+
 Pusher.logger = Rails.logger
-Pusher.url = Settings.pusher_url if Settings.pusher_url.present?
 Pusher.encrypted = true
